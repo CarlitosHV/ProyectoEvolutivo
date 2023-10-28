09adcb85f66a557c44fba8648513cda95e14c66e
CarlitosHV <carloshvsa21@hotmail.com>
2023-10-28 16:06:53 -0600

    Agregué la probabilidad de cruce, cambié el tipo de cruce a cruce uniforme y añadí la mutación con una probabilidad de 1%

--- Index.py
+++ Index.py
@@ -6,15 +6,18 @@ import matplotlib.pyplot as plt
 
 # Mi configuración del individuo e instancias
 num_recipes = 19
-num_persons = 20
+num_persons = 10
 recipes = rec.Recipes()
 supplies = ins.Insumos()
 maximo = 0
-num_generaciones = 5
+menor = 0
+num_generaciones = 20
 tam_torneo = 2
-puntos_cruce = [2, 8, 14, 18]
+promedios = []
+mejores = []
+menores = []
 
-# Población de prueba
+# Población de prueba a 10
 population_test = [
     [5, 7, 8, 8, 7, 7, 2, 5, 7, 6, 1, 5, 2, 4, 7, 7, 0, 2, 9],
     [3, 0, 0, 7, 4, 9, 5, 0, 3, 9, 5, 7, 3, 2, 0, 4, 4, 1, 2],
@@ -24,37 +27,29 @@ population_test = [
     [6, 9, 4, 9, 7, 6, 0, 2, 5, 3, 6, 4, 4, 8, 3, 6, 4, 4, 7],
     [2, 0, 0, 9, 9, 6, 1, 9, 0, 9, 0, 9, 6, 1, 4, 3, 3, 1, 0],
     [3, 3, 6, 0, 4, 9, 4, 5, 7, 3, 0, 0, 4, 2, 6, 0, 8, 3, 1],
-    [4, 3, 6, 2, 7, 6, 8, 8, 7, 2, 4, 5, 0, 8, 0, 3, 6, 7, 4],
-    [1, 7, 5, 1, 9, 1, 0, 3, 2, 2, 5, 8, 7, 8, 8, 5, 6, 4, 1],
-    [4, 9, 5, 2, 7, 9, 4, 0, 7, 9, 0, 4, 1, 2, 3, 9, 4, 0, 3],
-    [1, 4, 4, 6, 1, 4, 8, 6, 3, 2, 5, 6, 8, 1, 6, 7, 7, 5, 8],
-    [6, 5, 0, 2, 3, 9, 2, 3, 5, 7, 8, 9, 9, 2, 7, 8, 7, 0, 6],
-    [4, 1, 0, 8, 5, 0, 7, 3, 4, 1, 8, 9, 2, 5, 0, 4, 4, 8, 2],
-    [4, 3, 4, 3, 2, 4, 3, 3, 7, 0, 9, 3, 8, 5, 4, 1, 9, 7, 0],
-    [1, 6, 8, 1, 2, 6, 1, 4, 8, 9, 1, 4, 1, 2, 2, 5, 1, 7, 1],
-    [1, 2, 7, 7, 1, 5, 7, 9, 6, 2, 8, 3, 1, 3, 6, 1, 9, 6, 6],
-    [5, 8, 1, 2, 8, 5, 1, 0, 6, 4, 2, 2, 0, 8, 0, 9, 6, 2, 0],
-    [2, 7, 1, 4, 7, 3, 7, 7, 7, 2, 6, 2, 2, 9, 6, 0, 3, 3, 8],
-    [2, 5, 0, 3, 4, 3, 4, 9, 2, 7, 7, 1, 8, 3, 0, 4, 0, 1, 7],
+    [4, 3, 6, 2, 7, 6, 8, 8, 7, 2, 4, 5, 0, 8, 0, 3, 6, 7, 4]
 ]
 population = np.array(population_test)
+# population = np.array(0, 10, size=(num_persons, num_recipes))
+# Mutación entre 0 a 10, se muta, en un número aleatorio de 0 a 1000, y es gen por gen
 
 def funcion_aptitud(individuo):
     ganancias = 0
     merma = 0
     tiempo_total = 0
-
-    # Calculo las ganancias y el tiempo total
-    for i, (nombre_receta, cantidad) in enumerate(zip(recipes.recipes.keys(), individuo)):
-        ganancias += cantidad * recipes.obtener_recipes(nombre_receta)['Precio']
-        tiempo_total += cantidad * recipes.obtener_recipes(nombre_receta)['Tiempo elaboración']
-
-    # Si el tiempo total excede el límite, penalizamos la aptitud
-    if tiempo_total / 3 > 240:
-        return -1
-
-    # Calculo la merma
+    tiempo_limite = 3 * 240
     insumos_usados = {}
+
+    for i, (nombre_receta, cantidad) in enumerate(zip(recipes.recipes.keys(), individuo)):
+        precio_receta = recipes.obtener_recipes(nombre_receta)['Precio']
+        tiempo_receta = recipes.obtener_recipes(nombre_receta)['Tiempo elaboración']
+
+        if tiempo_total + cantidad * tiempo_receta > tiempo_limite:
+            cantidad = (tiempo_limite - tiempo_total) // tiempo_receta
+
+        ganancias += cantidad * precio_receta
+        tiempo_total += cantidad * tiempo_receta
+
     for i, (nombre_receta, cantidad) in enumerate(zip(recipes.recipes.keys(), individuo)):
         for insumo, cantidad_insumo in recipes.obtener_recipes(nombre_receta).items():
             if insumo not in insumos_usados:
@@ -67,95 +62,77 @@ def funcion_aptitud(individuo):
                 merma += (cantidad - supplies.obtener_insumo(insumo)['Cantidad']) * supplies.obtener_insumo(insumo)[
                     'Precio']
 
-    # La aptitud es igual a la merma menos la ganancia
     aptitud = merma - ganancias
-
     return aptitud
 
 
 def seleccion_torneo(poblacion, funcion_aptitud, tam_torneo=3):
-    # Selecciona aleatoriamente 'tam_torneo' individuos de la población
     seleccionados = random.sample(poblacion, tam_torneo)
-
-    # Evalúa la aptitud de los individuos seleccionados
     aptitudes = [funcion_aptitud(individuo) for individuo in seleccionados]
-
-    # Obtiene el índice del individuo con la mejor aptitud
-    indice_ganador = aptitudes.index(max(aptitudes))
-
-    # Retorna el individuo ganador del torneo
+    indice_ganador = aptitudes.index(min(aptitudes))
     return seleccionados[indice_ganador]
 
 
-def cruce_multipunto(padre1, padre2, puntos):
-    # Asegura que los puntos de cruce estén ordenados
-    puntos = sorted(puntos)
+def cruce(padre1, padre2):
+    hijo1 = np.zeros_like(padre1)
+    hijo2 = np.zeros_like(padre2)
 
-    # Inicializa la descendencia como listas vacías
-    hijo1 = []
-    hijo2 = []
-
-    # Realiza el cruce en cada segmento definido por los puntos de cruce
-    for i in range(len(puntos) - 1):
-        if i % 2 == 0:
-            # En segmentos pares, el hijo1 recibe genes del padre1 y el hijo2 del padre2
-            hijo1.extend(padre1[puntos[i]:puntos[i + 1]])
-            hijo2.extend(padre2[puntos[i]:puntos[i + 1]])
+    for i in range(len(padre1)):
+        if np.random.rand() < 0.5:
+            hijo1[i] = padre1[i]
+            hijo2[i] = padre2[i]
         else:
-            # En segmentos impares, el hijo1 recibe genes del padre2 y el hijo2 del padre1
-            hijo1.extend(padre2[puntos[i]:puntos[i + 1]])
-            hijo2.extend(padre1[puntos[i]:puntos[i + 1]])
+            hijo1[i] = padre2[i]
+            hijo2[i] = padre1[i]
 
-    # Asegura que los últimos genes sean añadidos a la descendencia
-    if len(puntos) % 2 == 0:
-        hijo1.extend(padre1[puntos[-1]:])
-        hijo2.extend(padre2[puntos[-1]:])
+    probabilidad = np.random.randint(0, 100)
+    if 0 < probabilidad <= 90:
+        return hijo1, hijo2
     else:
-        hijo1.extend(padre2[puntos[-1]:])
-        hijo2.extend(padre1[puntos[-1]:])
+        return padre1, padre2
 
-    return hijo1, hijo2
+def mutacion(individuo):
+    for i in range(len(individuo)):
+        probabilidad_mutacion = np.random.randint(0, 1001)
+        if probabilidad_mutacion <= 10:
+            individuo[i] = np.random.randint(0, 10)
 
-# Inicializa listas para almacenar el promedio y la mejor aptitud de cada generación
-promedios = []
-mejores = []
+    return individuo
+
 
 for generacion in range(num_generaciones):
     print(f'Generación {generacion + 1}')
 
-    # Calcula la aptitud de cada individuo en la población
     aptitudes = [funcion_aptitud(individuo) for individuo in population_test]
 
-    # Calcula el promedio y la mejor aptitud de la generación actual
     promedio = sum(aptitudes) / len(aptitudes)
     mejor = max(aptitudes)
+    menor = min(aptitudes)
 
-    # Añade el promedio y la mejor aptitud a las listas correspondientes
     promedios.append(promedio)
     mejores.append(mejor)
+    menores.append(menor)
 
     print(f'Promedio de aptitud: {promedio:.2f}')
     print(f'Mejor aptitud: {mejor:.2f}')
+    print(f'Peor aptitud: {menor:.2f}')
 
-    # Crea una nueva población a través de la selección y el cruce
     nueva_poblacion = []
     for _ in range(num_persons // 2):
-        # Selecciona dos padres utilizando el torneo de selección
         padre1 = seleccion_torneo(population_test, funcion_aptitud, tam_torneo)
         padre2 = seleccion_torneo(population_test, funcion_aptitud, tam_torneo)
 
-        # Crea dos hijos a través del cruce multipunto
-        hijo1, hijo2 = cruce_multipunto(padre1, padre2, puntos_cruce)
+        hijo1, hijo2 = cruce(padre1, padre2)
 
-        # Añade los hijos a la nueva población
+        hijo1 = mutacion(hijo1)
+        hijo2 = mutacion(hijo2)
+
         nueva_poblacion.extend([hijo1, hijo2])
-
-    # Reemplaza la población antigua con la nueva población
     population_test = nueva_poblacion
 
-# Crea una gráfica de líneas con los promedios y las mejores aptitudes
 plt.plot(promedios, label='Promedio')
-plt.plot(mejores, label='Mejor')
+plt.plot(menores, label='Mejores')
+plt.plot(mejores, label='Peores')
 plt.xlabel('Generación')
 plt.ylabel('Aptitud')
 plt.legend()
